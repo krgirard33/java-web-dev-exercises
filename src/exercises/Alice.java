8f03dff31a35b053c605627397645705bc3d65ce
krgirard33 <ken.girard@gmail.com>
2023-01-03 00:03:40 -0600

    Alice now tells ignores case on search, also prints word as well as location, and replaces all double spaces with single

--- src/exercises/Alice.java
+++ src/exercises/Alice.java
@@ -4,20 +4,39 @@ import java.util.Scanner;
 
 public class Alice {
     public static void main(String[] argv){
-        String text = "Alice was beginning to get very tired of sitting by her sister " +
+        String textToSearch = "Alice was beginning to get very tired of sitting by her sister " +
                 "on the bank, and of having nothing to do: once or twice she had peeped " +
                 "into the book her sister was reading, but it had no pictures or " +
                 "conversations in it, 'and what is the use of a book,' thought Alice " +
                 "'without pictures or conversation?'";
+
         Scanner input = new Scanner(System.in);
+        // Get the word
         System.out.println("What word would you like to look for?");
-        String word = input.nextLine();
-        int i = text.indexOf(word);
-        System.out.println((i>0));
+        String word = input.nextLine().toLowerCase();
+        input.close();
+        // word = word.toLowerCase();
+
+        // finds the 1st occurrence of word in the text
+        int ind = textToSearch.toLowerCase().indexOf(word);
+
+        // gives true/false on if word is in the text
+        // System.out.println((ind>0));
+        if(ind>0) {
+            //if word found
+            System.out.println(word + " starts at " + ind + " and is " + word.length() + " letters long.");
+            /*
+            If we wanted to print out what was at that index location
+             System.out.println(str.substring(i, i+word.length()));
+             */
+            // removes ist occurrence of word
+            textToSearch = textToSearch.replaceFirst(word,"");
+            // removes all double spaces, replaces with single space. store in a new String
+            String wordRemoved = textToSearch.replaceAll("  ", " ");
+            System.out.println(wordRemoved);
+        }
+        else{
+            System.out.println("Word not found");
+        }
     }
-
-
-
-
-
 }
