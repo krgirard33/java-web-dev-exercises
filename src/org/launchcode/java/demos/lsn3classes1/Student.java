abf7db882030ddbfce8de77232fe6fb0f54c4685
krgirard33 <ken.girard@gmail.com>
2023-01-05 23:27:48 -0600

    I told it to clean up my files... and it picked these up as well.

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -7,7 +7,7 @@ public class Student {
 
     private String name;
     private int studentId;
-    private int numberOfCredits = 0;
-    private double gpa = 0.0;
+    private final int numberOfCredits = 0;
+    private final double gpa = 0.0;
 
 }
