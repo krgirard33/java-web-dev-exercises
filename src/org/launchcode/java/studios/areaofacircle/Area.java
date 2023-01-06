973d2d764f095a5c09264efbcda1a522cfcc56fb
krgirard33 <ken.girard@gmail.com>
2023-01-05 23:35:02 -0600

    Area now ask if you want it to run again

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -5,21 +5,26 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args) {
         double radius = 0;
-
-        while (radius < 1 ) {
-            Scanner input = new Scanner(System.in);
-            System.out.println("What is the radius of the circle?");
-            while(input.hasNextDouble()) {
-                radius = input.nextDouble();
-                //input.close();
-                if (radius > 0) {
-                    double areaOfCircle = Circle.getArea(radius);
-                    System.out.println("The area of a circle with a " + radius + " radius is " + areaOfCircle);
-                } else {
-                    System.out.println("Please enter a valid number.");
-                    main(args);
+        int stop = 0;
+        Scanner input = new Scanner(System.in);
+        do {
+            radius = 0;
+            while (radius < 1) {
+                System.out.println("What is the radius of the circle?");
+                if (input.hasNextDouble()) {
+                    radius = input.nextDouble();
+                    if (radius > 0) {
+                        double areaOfCircle = Circle.getArea(radius);
+                        System.out.println("The area of a circle with a " + radius + " radius is " + areaOfCircle);
+                    } else {
+                        System.out.println("Please enter a valid number.");
+                        main(args);
+                    }
                 }
             }
-        }
+            System.out.println("Would you like to run again? (1 to run or 0 to end)");
+            stop = input.nextInt();
+        } while (stop == 1);
+        input.close();
     }
 }
