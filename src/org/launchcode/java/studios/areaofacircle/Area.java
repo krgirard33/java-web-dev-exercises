1bae622fa0f9f122abf9b2c9b46adc6f2b0614b8
krgirard33 <ken.girard@gmail.com>
2023-01-05 18:58:57 -0600

    Created studio folder, areaofcircle package, Area & Circle

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
-        Double pi = 3.14;
+        double radius = 0;
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("What is the radius of the circle?");
-        Double radius = input.nextDouble();
-        input.close();
-        Double areaOfCircle = radius * radius * pi;
-        System.out.println("The area of a circle with a "+radius+" radius is "+areaOfCircle);
+        while (radius < 1 ) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("What is the radius of the circle?");
+            radius = input.nextDouble();
+            //input.close();
+            if (radius > 0) {
+                double areaOfCircle = Circle.getArea(radius);
+                System.out.println("The area of a circle with a " + radius + " radius is " + areaOfCircle);
+            }
+        }
     }
 }
