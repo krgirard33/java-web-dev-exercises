2adcbaa03565c00ad493bc51c46b26118da5336c
krgirard33 <ken.girard@gmail.com>
2023-01-08 16:28:08 -0600

    Now prints each odd index value

--- src/exercises/chap3/ArrayPractice.java
+++ src/exercises/chap3/ArrayPractice.java
@@ -4,8 +4,8 @@ public class ArrayPractice {
     public static void main(String[] args) {
         int[] intArray = {1, 1, 2, 3, 5, 8};
 
-        for(int num : intArray) {
-            System.out.println(num);
+        for(int i=0; i<intArray.length;i+=2) {
+            System.out.println(intArray[i]);
         }
     }
 }
