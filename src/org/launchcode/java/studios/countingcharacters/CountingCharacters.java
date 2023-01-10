ff505fbfc2978ec13c7944b7f58168fdf54e536a
krgirard33 <ken.girard@gmail.com>
2023-01-09 20:01:00 -0600

    Moved the user input to its own method, created a system to let the user pick which of the 3 methods to use.

--- src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
+++ src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
@@ -10,25 +10,50 @@ public class CountingCharacters {
 
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
-        System.out.println();
-        String userstring;
+        System.out.println("Count characters using: 0) Built in text, (1) User input text, (2) Text on a file");
+        int choice = input.nextInt();
+
+        //String userstring;
         String fileName = "src/org/launchcode/java/studios/countingcharacters/fromfile.txt";
 
         String hiddenfig = "If the product of two terms is zero then common sense says at least one of the two terms " +
                 "has to be zero to start with. So if you move all the terms over to one side, you can put the " +
                 "quadratics into a form that can be factored allowing that side of the equation to equal zero. Once " +
                 "you’ve done that, it’s pretty straightforward from there.";
+        //characterCount(hiddenfig);
 
+        /*
         System.out.println("Enter text you wish counted:");
         userstring = input.nextLine();
         input.close();
+        characterCount(userstring);
+        */
 
-        //characterCount(userstring);
+        //userCount("cat");
+
+        switch (choice) {
+            case 0:
+                System.out.println("Built in text");
+                characterCount(hiddenfig);
+                break;
+            case 1:
+                userCount("cat");
+                break;
+            case 2:
+                try {
+                    readFile(fileName);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+        }
+        /*
         try {
             readFile(fileName);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        */
 
     }
 
@@ -52,7 +77,18 @@ public class CountingCharacters {
         Path path = Paths.get(filename);
         Scanner scanner = new Scanner(path);
         String line = scanner.nextLine();
+        System.out.println("Reading from a file");
         characterCount(line);
         scanner.close();
+    }
+
+    public static void userCount(String args) {
+        Scanner input = new Scanner(System.in);
+        String userstring;
+        System.out.println("Enter text you wish counted:");
+        userstring = input.nextLine();
+        input.close();
+        System.out.println("Reading from user input");
+        characterCount(userstring);
     }
 }
