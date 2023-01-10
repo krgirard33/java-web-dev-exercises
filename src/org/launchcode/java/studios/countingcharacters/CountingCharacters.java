9f080096348a1636ccbae93ba0cbabb7cf5cdae0
krgirard33 <ken.girard@gmail.com>
2023-01-09 22:56:23 -0600

    Added default to choice switch, removed a bunch of commented out code, copied alphabet method

--- src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
+++ src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
@@ -13,23 +13,13 @@ public class CountingCharacters {
         System.out.println("Count characters using: 0) Built in text, (1) User input text, (2) Text on a file");
         int choice = input.nextInt();
 
-        //String userstring;
         String fileName = "src/org/launchcode/java/studios/countingcharacters/fromfile.txt";
 
         String hiddenfig = "If the product of two terms is zero then common sense says at least one of the two terms " +
                 "has to be zero to start with. So if you move all the terms over to one side, you can put the " +
                 "quadratics into a form that can be factored allowing that side of the equation to equal zero. Once " +
                 "you’ve done that, it’s pretty straightforward from there.";
-        //characterCount(hiddenfig);
 
-        /*
-        System.out.println("Enter text you wish counted:");
-        userstring = input.nextLine();
-        input.close();
-        characterCount(userstring);
-        */
-
-        //userCount("cat");
 
         switch (choice) {
             case 0:
@@ -46,27 +36,24 @@ public class CountingCharacters {
                     throw new RuntimeException(e);
                 }
                 break;
+            default:
+                System.out.println("Please enter a valid choice.");
         }
-        /*
-        try {
-            readFile(fileName);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        */
-
     }
 
     public static void characterCount(String args){
         HashMap<Character, Integer> charMap = new HashMap<>();
-        args = args.toUpperCase().replaceAll("\\p{IsPunctuation} | \\p{Space}", "");
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        args = args.toUpperCase();
         char[] charactersInString = args.toCharArray();
 
         for (char c : charactersInString) {
-            if (charMap.containsKey(c)) {
-                charMap.put(c, charMap.get(c) + 1);
-            } else {
-                charMap.put(c, 1);
+            if (alphabet.indexOf(c) >= 0) {
+                if (charMap.containsKey(c)) {
+                    charMap.put(c, charMap.get(c) + 1);
+                } else {
+                    charMap.put(c, 1);
+                }
             }
         }
         System.out.println("Character Count");
@@ -77,9 +64,9 @@ public class CountingCharacters {
         Path path = Paths.get(filename);
         Scanner scanner = new Scanner(path);
         String line = scanner.nextLine();
+        scanner.close();
         System.out.println("Reading from a file");
         characterCount(line);
-        scanner.close();
     }
 
     public static void userCount(String args) {
