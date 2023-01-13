70a32db9670b954df086d94e6603e24b95682d36
krgirard33 <ken.girard@gmail.com>
2023-01-13 14:30:05 -0600

    Student class: added getters, setters, constructor

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -7,7 +7,43 @@ public class Student {
 
     private String name;
     private int studentId;
-    private final int numberOfCredits = 0;
-    private final double gpa = 0.0;
+    private final int numberOfCredits;
+    private final double gpa;
 
+    // NAME
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // STUDENT ID
+    public int getStudentId() {
+        return studentId;
+    }
+    private void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    // CREDITS
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    // GPA
+    public double getGpa() {
+        return gpa;
+    }
+
+    public Student(String name, int studentId, int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public Student(String name, int studentId) {
+        this(name, studentId, 0, 0);
+    }
 }
